a7112000289d1dbab57a5388a92c2880e753bac9
Josh Ginnett <jmginnett100@gmail.com>
2025-08-10 11:39:40 -0400

    deleted useless comment

--- exporter.py
+++ exporter.py
@@ -7,36 +7,6 @@ class Exporter:
     def __init__(self, simulation):
         self.sim = simulation
 
-    # def export_to_measur(self):
-    #     """
-    #     Exports system profile data to MEASUR using pyautogui and user input.
-    #     """
-    #     print("Export Process Starting...")
-    #     print("TO EXIT PROGRAM WHILE WRITING, MOVE MOUSE TO TOP LEFT CORNER")
-    #     time.sleep(1)
-
-    #     for day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
-    #         print('-'*80)
-    #         print(f"\033[91mPLEASE ENSURE:\033[0m")
-    #         print(f"\033[93mSelect Day Type\033[0m is set to \033[93m{day.upper()}\033[0m")
-    #         print(f"\033[93mProfile Data Type\033[0m is set to \033[93mPower, kW\033[0m")
-    #         print(f"\033[94mPress 'y' to continue...\033[0m")
-    #         keyboard.wait('y')
-
-    #         for comp in self.sim.get_compressors():
-    #             if day not in comp.data:
-    #                 continue  # skip this compressor for this day
-
-    #             print(f"\033[91mPLEASE ENSURE:\033[0m The \033[93m0:00\033[0m field for \033[93m{comp.name}\033[0m is selected")
-    #             print(f"\033[94mPress 'y' to continue...\033[0m")
-    #             keyboard.wait('y')
-
-    #             for value in comp.data[day].values():
-    #                 pyautogui.typewrite(str(value))
-    #                 pyautogui.press('tab')
-
-    #     print(f"\033[92mEXPORT SUCCESSFUL\033[0m")
-
     def export_to_measur(self, day, export_compressors):
         """
         Exports a data for a given day and order of compressors to MEASUR using
